f6491314b2e23392bcc51714ab93aa4c99566cc4
Nicolas Cueva <nicolascueva@outlook.com>
2018-09-05 22:23:03 -0500

    adds some inquirer

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -17,6 +17,44 @@ var connection = mysql.createConnection({
 
 connection.connect(function (err) {
     if (err) throw err;
-    console.log("connected as id " + connection.threadId + "\n");
+    //console.log("connected as id " + connection.threadId + "\n");
     // function to start here
+    displayProducts();
+    //start();
 });
+
+function displayProducts() {
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err;
+        console.log("\n" + "---------------------------------");
+        for (var i = 0; i < res.length; i++) {
+
+            console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].price);
+        }
+        console.log("-----------------------------------");
+        //console.log(res);
+        //connection.end();
+    });
+    start();
+};
+
+function start() {
+
+    inquirer
+        .prompt([
+            {
+                name: "selectedProduct",
+                type: "integer",
+                message: "Which product would you like to buy? Please select by ID"
+            },
+            {
+                name: "unitsBought",
+                type: "integer",
+                message: "How many would you like to buy?"
+            }
+        ])
+        .then(function (answer) {
+            console.log(answer);
+        });
+
+};
